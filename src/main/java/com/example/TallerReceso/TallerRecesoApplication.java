b2cee426af155a087dd454beb6d419e0f6a3a080
Juan Leal <lecrujuan@gmail.com>
2025-10-21 23:19:36 -0500

    feat: Se edita el pom con las dependencias necesarias que ya hemos usado antes

---
+++ src/main/java/com/example/TallerReceso/TallerRecesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TallerReceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerRecesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerRecesoApplication.class, args);
+	}
+
+}
